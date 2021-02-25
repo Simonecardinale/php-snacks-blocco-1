52df56216bc845edfbae8500f7da8c1822af14d6
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-25 16:11:20 +0100

    work complete + mini bonus
    checking in snack 2 if string after "@" is longer than 3 words

--- snack2/index.php
+++ snack2/index.php
@@ -2,9 +2,10 @@
     $nome = $_GET['name'];
     $email = $_GET['mail'];
     $eta = $_GET['age'];
+    $ciao = "ciao@tutti.com";
+    $checkAfterAt = preg_split("/[@\s.]/", $email);
 
-
-    if(strlen($nome) > 3 && strpos($email, ".") && strpos($email, "@") && is_numeric($eta)) {
+    if(strlen($nome) > 3 && strpos($email, ".") && strpos($email, "@") && is_numeric($eta) && strlen($checkAfterAt[1]) > 3) {
         echo "Accesso riuscito";
     } else {
         echo "Accesso negato";
