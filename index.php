42a489e75544f7a201a936a3793e86834ba3930f
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-25 15:27:21 +0100

    snack 1 WIP
    For loop complete

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+    $matches = [
+        [
+            "casa" => "Los Angeles Lakers",
+            "ospite" => "Golden State Warriors",
+            "punteggioCasa" => 104, 
+            "punteggioOspite" => 60,
+        ],
+        [
+            "casa" => "Cleveland Cavaliers",
+            "ospite" => "Chicago Bulls",
+            "punteggioCasa" => 85,
+            "punteggioOspite" => 36,
+        ],
+        [
+            "casa" => "Miami Heat",
+            "ospite" => "Philadelphia 76ers",
+            "punteggioCasa" => 125,
+            "punteggioOspite" => 84,
+        ],
+        [
+            "casa" => "Milwaukee Bucks",
+            "ospite" => "San Antonio Spurs",
+            "punteggioCasa" => 89,
+            "punteggioOspite" => 80,
+        ],
+    ];
+?>
+
+<?php 
+    for ($i = 0; $i < count($matches); $i++) { 
+    $casa = $matches[$i]['casa'];  
+    $ospite =  $matches[$i]['ospite'];
+    $punteggioCasa = $matches[$i]['punteggioCasa'];
+    $punteggioOspite = $matches[$i]['punteggioOspite'];
+?>
+
+
+
+<?php }?>
