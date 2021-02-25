510546cce00424d56303d7e05ecc7f940bed6f57
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-25 17:43:35 +0100

    work complete + bonus
    Snack 1: showing which variable is wrong
    snack2: checking if string after "@" is longer than 3 words

--- snack2/index.php
+++ snack2/index.php
@@ -4,10 +4,40 @@
     $eta = $_GET['age'];
     $ciao = "ciao@tutti.com";
     $checkAfterAt = preg_split("/[@\s.]/", $email);
+    $checkarray = [
+        [
+            $checkname = true,
+            "errore" => "Attenzione: nome errato!"
+        ],
+        [
+            $checkemail = true,
+            "errore" => "Attenzione: email errata!"
+        ],
+        [
+            $checkage = true,
+            "errore" => "Attenzione: età errata!"
+        ],
+    ];
+
 
     if(strlen($nome) > 3 && strpos($email, ".") && strpos($email, "@") && is_numeric($eta) && strlen($checkAfterAt[1]) > 3) {
         echo "Accesso riuscito";
-    } else {
-        echo "Accesso negato";
+    } 
+    if (strlen($nome) <= 3) {
+        $checkarray[0][0] = false;
+    } 
+    if (!strpos($email, ".") || !strpos($email, "@") || strlen($checkAfterAt[1]) < 3) {
+        $checkarray[1][0] = false;
+    } 
+    if (!is_numeric($eta)) {
+        $checkarray[2][0] = false;
+    };
+
+    for ($i = 0; $i < count($checkarray); $i++) {
+        if($checkarray[$i][0] == false) {
+            echo $checkarray[$i]["errore"];
+        }
     }
+    
+
 ?>
